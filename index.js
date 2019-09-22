47c17bb2156126ddf4860977292953cfd8e48115
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-22 03:15:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // Code your solution in this file.
-// function lowerCaseDrivers(driversArray) {
-//   return driversArray.map(function(driverName) {
-//     return driverName.toLowerCase()
-//   })
-// }
 
 // function nameToAttributes(driversArray) {
 //   return driversArray.map(function(fullName) {
