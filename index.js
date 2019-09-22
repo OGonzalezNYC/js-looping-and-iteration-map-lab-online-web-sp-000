52ac9cb695036407231b81f4a79f73de0c537394
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-22 02:40:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 // Code your solution in this file.
-function lowerCaseDrivers(driversArray) {
-  return driversArray.map(function(driverName) {
-    return driverName.toLowerCase()
+// function lowerCaseDrivers(driversArray) {
+//   return driversArray.map(function(driverName) {
+//     return driverName.toLowerCase()
+//   })
+// }
+
+// function nameToAttributes(driversArray) {
+//   return driversArray.map(function(fullName) {
+//     return {firstName: fullName.split(" ")[0], lastName: fullName.split(" ")[1]}
+//   })
+// }
+
+// function attributesToPhrase(driversArray) {
+//   return driversArray.map(function(object) {
+//     return `${object.name} is from ${object.hometown}`
+//   })
+// }
+function lowerCaseDrivers(array) {
+  return array.map(function(element) {
+    return element.toLowerCase();
   })
 }
 
-function nameToAttributes(driversArray) {
-  return driversArray.map(function(fullName) {
-    return {firstName: fullName.split(" ")[0], lastName: fullName.split(" ")[1]}
-  })
-}
+// const drivers = [
+//         'Bobby Smith',
+//         'Sammy Watkins',
+//         'Sally Jenkins',
+//         'Annette Sawyer',
+//         'Sarah Hucklebee',
+//         'bobby ridge'
+//          ]
 
-function attributesToPhrase(driversArray) {
-  return driversArray.map(function(object) {
-    return `${object.name} is from ${object.hometown}`
-  })
-}
+// [
+//         { firstName: 'Bobby', lastName: 'Smith' },
+//         { firstName: 'Sammy', lastName: 'Watkins' },
+//         { firstName: 'Sally', lastName: 'Jenkins' },
+//         { firstName: 'Annette', lastName: 'Sawyer' },
+//         { firstName: 'Sarah', lastName: 'Hucklebee' },
+//         { firstName: 'bobby', lastName: 'ridge' }
+//       ]
 
+function nameToAttributes(array) {
+  array.map(function(element) {
+     let newElement = {};
+     let i = 0;
+     while (i < element.length);
+     
+     if (indexOf(element[i]) < indexOf(" ")) {
+       newElement[i].firstName.push(element[i]);
+     } if (indexOf(element[i]) > indexOf(" ")) {
+       newElement[i].lastName.push(element[i]);
+     }
+     i++;
+     return newElement;
+   });
+}
