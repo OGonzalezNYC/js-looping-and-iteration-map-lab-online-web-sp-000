343489db7227ea26e85b8c732465541c18b0cad1
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-22 03:20:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ function lowerCaseDrivers(array) {
   })
 }
 
-// function nameToAttributes(array) {
-//   return array.map(function(element) {
-//     return {firstName: element.split(" ")[0], lastName: element.split(" ")[1]}
-//   })
-// }
 function nameToAttributes(array) {
   return array.map(function(element) {
-    let newElement = {firstName: "", lastName: ""};
-    let i = 0;
-    while (i < element.length) {
-    if (i < element.indexOf(" ")) {
-      newElement.firstName = newElement.firstName + element[i];
-    }
-    if (i > element.indexOf(" ")) {
-      newElement.lastName = newElement.lastName + element[i];
-    }
-    i++;
-  } return newElement;
-  });
+    return {firstName: element.split(" ")[0], lastName: element.split(" ")[1]}
+  })
 }
+// function nameToAttributes(array) {
+//   return array.map(function(element) {
+//     let newElement = {firstName: "", lastName: ""};
+//     let i = 0;
+//     while (i < element.length) {
+//     if (i < element.indexOf(" ")) {
+//       newElement.firstName = newElement.firstName + element[i];
+//     }
+//     if (i > element.indexOf(" ")) {
+//       newElement.lastName = newElement.lastName + element[i];
+//     }
+//     i++;
+//   } return newElement;
+//   });
+// }
 
 function attributesToPhrase(array) {
   return array.map(function(element) {
