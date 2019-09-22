e86405faa58e19cc56f2e24cad7184d033b83c8e
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-22 03:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ function nameToAttributes(array) {
     return {firstName: element.split(" ")[0], lastName: element.split(" ")[1]}
   })
 }
-// function nameToAttributes(array) {
-//   return array.map(function(element) {
-//     let newElement = {firstName: "", lastName: ""};
-//     let i = 0;
-//     while (i < element.length) {
-//     if (i < element.indexOf(" ")) {
-//       newElement.firstName = newElement.firstName + element[i];
-//     }
-//     if (i > element.indexOf(" ")) {
-//       newElement.lastName = newElement.lastName + element[i];
-//     }
-//     i++;
-//   } return newElement;
-//   });
-// }
+function nameToAttributes(array) {
+  return array.map(function(element) {
+    let newElement = {firstName: "", lastName: ""};
+    let i = 0;
+    while (i < element.length) {
+    if (i < element.indexOf(" ")) {
+      newElement.firstName = newElement.firstName + element[i];
+    }
+    if (i > element.indexOf(" ")) {
+      newElement.lastName = newElement.lastName + element[i];
+    }
+    i++;
+  } return newElement;
+  });
+}
 
 function attributesToPhrase(array) {
   return array.map(function(element) {
